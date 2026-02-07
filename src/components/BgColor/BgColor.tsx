adb3244c3da48690e1f164f48812f6f1699c810a
exrynome <fooky.yt@gmail.com>
2026-02-08 01:41:33 +0300

    animation

--- src/components/BgColor/BgColor.tsx
+++ src/components/BgColor/BgColor.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {FunctionComponent} from "react";
+import { FunctionComponent } from "react";
 import styles from './BgColor.module.scss'
 import useTheme from "@/components/ThemeSelector/internal";
 
@@ -18,18 +18,10 @@ const BgColor: FunctionComponent<Props> = ({ color, position = 'below' }) => {
 
     return (
         <div className={`${styles.wrapper} ${styles[position]}`}>
-            <div className={`${styles.container} ${styles[color]}`}>
-                <svg
-                    className={`${styles.wave} ${styles.wave1} ${styles[color]}`}
-                    width="2410"
-                    height="837"
-                    viewBox="0 0 2410 837"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path d="M612.192 209.425C427.587 60.4034 127.146 86.8717 0 118.733L177.092 803.138H1214.07L2147.47 837L2409.55 352.444C2415.18 344.555 2370.64 285.219 2147.47 110.978C1868.48 -106.823 1450 33.5969 1318.82 210.277C1187.66 386.957 842.949 395.7 612.192 209.425Z" fill="currentColor"/>
-                </svg>
-
+            <div className={`${styles.blurContainer} ${styles[color]}`}>
+                <div className={styles.rectangle} />
+                <div className={`${styles.circle} ${styles.circle__1}`} />
+                <div className={`${styles.circle} ${styles.circle__2}`} />
             </div>
         </div>
     )
